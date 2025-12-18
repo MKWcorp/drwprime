48b10a949fdc2624fd7258684ea3179c3ce66fda
multimediadrw <multimediadrw@gmail.com>
2025-12-18 12:18:06 +0700

    Add Google Tag Manager (GTM-NVD6JXKN)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="id" className={`${inter.variable} ${playfair.variable}`}>
         <head>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NVD6JXKN');`}
+          </Script>
           <Script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-Z4M7H1T0NQ"
@@ -48,6 +55,14 @@ export default function RootLayout({
           </Script>
         </head>
         <body className="antialiased">
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NVD6JXKN"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           {children}
         </body>
       </html>
